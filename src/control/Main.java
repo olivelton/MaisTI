12358bb3fb1bd68e9b0ae7269bf6362f4f5fedaa
root <root@acerDebian.acerDebian>
2017-10-23 19:56:16 -0200

    recomeçando projeto do zero

--- src/control/Main.java
+++ src/control/Main.java
@@ -2,13 +2,16 @@
 
 package control;
 
-import view.Loading;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// chama tela de carregar e passa o tempo de 3000 ms ou seja 3 segundo
-		new Loading(3000);
+
 	}
+
+
+
+
+
 
 }
