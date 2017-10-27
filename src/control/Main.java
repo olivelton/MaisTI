766a439cde44db019179c1a523759a87125f8f05
root <root@acerDebian.acerDebian>
2017-10-27 15:57:36 -0200

    projeto recomeçado criado tela splash funcionando + banco criado e populado cidades estados

--- src/control/Main.java
+++ src/control/Main.java
@@ -3,15 +3,42 @@
 package control;
 
 
+import view.CustomSplashScreen;
+
 public class Main {
 
+    /*Manifest-Version: 1.0
+SplashScreen-Image: logo.png
+X-COMMENT: Main-Class will be added automatically by build*/
+
+
 	public static void main(String[] args) {
+       loading();
+
+
+
+    }
+
+
+
+
+
+    //metodo para trabalhar com chamda da classe de splash
+    //loading nome pq sera o que vai carregar o logotipo antes de iniciar
+
+    static void loading(){
+        CustomSplashScreen.splashInit();
+
+        for (int i = 0; i < 10; i++) {
+            CustomSplashScreen.splashProgress(i * 20);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
 	}
 
 
-
-
-
-
-}
